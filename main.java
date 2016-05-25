16dd0a9af81b2d3703c097adfe2142fa88d39898
quantumSpark07 <c19ac1@dalton.org>
2016-05-25 12:19:31 -0400

    fixed bugs
    thanks to @thatoddmailbox for help

--- main.java
+++ main.java
@@ -16,53 +16,55 @@ import org.dalton.DaltonStock;
 import org.dalton.DaltonWii;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter; 
 import java.io.IOException;
 
 public class stockSim {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		//declare variables
 		String input =  "";
 		String choice = "";
-		
+
 		//set starting money
 		int money = 50000;
-		
+
 		//declare frame and Wii remote
 		DaltonDraw frame = new DaltonDraw();
 		DaltonWii mote = new DaltonWii("002147EE2B3E");
 
 		//intialize stock variables
-		DaltonStock apple = new DaltonStock("AAPL");
-		
+		DaltonStock stock = new DaltonStock("AAPL");
+
 
 		while(true) {
 			frame.drawString("Welcome to the Stock Simulator 3000", 350, 100, 50, Color.red);
-			
+
 			//NOTE: use AAPL stock only for now
 			frame.drawString("Buy a Stock", 100, 300, 50, Color.blue);
 			frame.drawString("Check your Portfolio", 400, 300, 50, Color.blue);			
 			frame.render();
 			input = mote.next(); //get input
 			frame.drawString(input, 550, 500, 100, Color.red);
-			
+
 			//if user swipes left w/ mote
 			if (input.equals("left")) {
 				//clears frame
 				frame.clear();
 				//ask user how many shares to buy - LATER FEATURE TO ADD
 				//frame.drawString("How many shares would you like to purchase?", 100, 300, 50, Color.blue);
-				
+
 				frame.drawString("Confirmation: You would like to buy 1 share of Apple?(press 'A' to confirm, 'B' to go back.", 550, 500, 100, Color.blue);
 				frame.render();
-				
+
 				input = mote.next();
 				if(input.equals("A")) {
 					frame.clear();
-					money = money - stock.lastPrice();
+					money = (int) (money - stock.lastprice);
 					//displays current till
 					frame.drawString("You now own 1 share of Apple. Current till: $" + money + ".", 100, 300, 50, Color.blue);
 					//saves data of current till and amount of shares bought to data file
@@ -71,32 +73,37 @@ public class stockSim {
 						BufferedWriter bufferedWriter = new BufferedWriter(writer);
 						bufferedWriter.write(money);
 						bufferedWriter.write("\n\r");
-						bufferedWriter.write(stock.lastPrice());
+						bufferedWriter.write((int) stock.lastprice);
 						bufferedWriter.close();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
+
 				//if user swipes right with mote
 				if(input.equals("right")) {
 					frame.clear();
-				
+
 					//pulls txt file with stored data and reads it
 					FileReader reader = new FileReader("portfolio.txt");
-					BufferedReader bufferedreader = new BufferedReader(reader);
-					
+					BufferedReader bufferedReader = new BufferedReader(reader);
+
 					String line;
-					
-					while((line = bufferedReader.readLine()) != null) {
-						//show portfolio
-						frame.drawString(line, 550, 500, 100, Color.blue);
-					}
-					reader.close();
-				} catch(IOException e) {
-					e.printStackTrace();
+
+					try {
+						while((line = bufferedReader.readLine()) != null) {
+							//show portfolio
+							frame.drawString(line, 550, 500, 100, Color.blue);
+							reader.close(); 
+						}
+					} catch(IOException e) {
+						e.printStackTrace();		
+					} 
 				}
+
+
 				frame.render();
-				
+
 			}
 		}
 
